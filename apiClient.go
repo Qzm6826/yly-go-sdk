edce5540c946b04feca296b6ecf79c22774bcb96
lwy <1139038165@qq.com>
2020-07-10 14:55:01 +0800

    修改：     apiClient.go 	修改：     apiUtil.go 	修改：     config.go

--- apiClient.go
+++ apiClient.go
@@ -3,6 +3,7 @@ package ylyOpenApi
 type ApiClient struct {
 	PrintService PrintService
 	SetPrinter   SetPrinter
+	config       Config
 }
 
 type PrintService struct {
@@ -11,4 +12,16 @@ type PrintService struct {
 
 type SetPrinter struct {
 	config *Config
+}
+
+func NewClient(config Config) ApiClient{
+	client := ApiClient{}
+	client.SetConfig(config)
+	return client
+}
+
+func (client *ApiClient) SetConfig(config Config) {
+	client.config = config
+	client.PrintService.config = &client.config
+	client.SetPrinter.config = &client.config
 }
